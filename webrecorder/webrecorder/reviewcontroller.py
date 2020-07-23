664ee836131f9bb91bf816caaa93719ee926c022
NHG <nhg@posteo.de>
2020-07-23 16:41:48 +0200

    reviewcontroller

---
+++ webrecorder/webrecorder/reviewcontroller.py
@@ -0,0 +1,22 @@
+from webrecorder.basecontroller import BaseController, wr_api_spec
+from bottle import request, response
+
+
+# ============================================================================
+class ReviewController(BaseController):
+    def init_routes(self):
+        # Review
+        wr_api_spec.set_curr_tag('review')
+
+        @self.app.get('/api/v1/review')
+        @self.api(resp='reviewcolls')
+        def get_review():
+            return ("test")
+
+        @self.app.post('/api/v1/review')
+        @self.api(query=[],
+                  req_desc='review coll',
+                  req=['user', 'coll_id'])
+        def post_review(req):
+            print(request.json)
+            return ("OK")
