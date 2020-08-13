6152721b7fb7457e9684869421abadc77ad43dd3
NHG <nhg@posteo.de>
2020-08-13 15:36:00 +0200

    get coll

--- webrecorder/webrecorder/reviewcontroller.py
+++ webrecorder/webrecorder/reviewcontroller.py
@@ -15,7 +15,7 @@ class ReviewController(BaseController):
             j = list()
             for r in self.redis.smembers('review'):
                 j.append(json.loads(r))
-                print(self.user_manager.all_users.make_user(j[0]))
+                print(self.user_manager.all_users.make_user(j[0]).get_collection_by_name(j[1]))
             return json.dumps(j)
 
         @self.app.post('/api/v1/review')
